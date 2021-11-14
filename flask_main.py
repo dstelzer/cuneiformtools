7de96f24d6707e5ee0d5e1d6dc7a0f2154e3fcb0
Daniel Stelzer <dsdraco7@gmail.com>
2021-11-14 12:48:19 -0600

    remove traces of old research survey

--- flask_main.py
+++ flask_main.py
@@ -79,20 +79,3 @@ def do_galdubsar():
 	rendparams = {'bgcolor':bgcolor, 'fgcolor':fgcolor, 'hlcolor':hlcolor, 'strokewidth':strokewidth, 'fill':fill}
 	layoutparams = {'justify':justify, 'size':size, 'margin':margin, 'leading':leading, 'spacing':spacing, 'kerning':kerning, 'absolute':absolute, 'fixed':fixedwidth}
 	return do_scribing(text, rendname=rend, format=format, rendparams=rendparams, layoutparams=layoutparams)
-
-@app.route('/research_survey')
-def do_research_survey():
-	from listmaker import make_list
-	which = request.args.get('which', '', type=str)
-	return make_list(which)
-
-@app.route('/submit_survey', methods=['POST'])
-def do_submit_survey():
-	import json
-	data = dict(request.form)
-	with open('tmp_survey.json', 'r') as f:
-		full = json.load(f)
-	full.append(data)
-	with open('tmp_survey.json', 'w') as f:
-		json.dump(full, f)
-	return f'Data submitted successfully! We now have {len(full)} data points. Thank you!'
