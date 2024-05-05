523a0234a9454a53435e25e609b4298d0bd5d4ec
Daniel Stelzer <dsdraco7@gmail.com>
2024-05-04 21:12:31 -0500

    update fonts code to properly handle ligatures and star variants

--- hantatallas/fonts.py
+++ hantatallas/fonts.py
@@ -7,8 +7,42 @@ from tqdm import tqdm
 from render import *
 import parser
 
+def glyph_name(codepoint): # Give a consistent name to each codepoint
+	return f'U{codepoint:04x}'
+
+SANITIZE = { # FontForge doesn't like certain characters in glyph names
+	' ' : '_',
+	'(' : '',
+	')' : '',
+	'.' : '_',
+	',' : '',
+	'/' : '_',
+	'Á' : 'A2',
+	'À' : 'A3',
+	'É' : 'E2',
+	'È' : 'E3',
+	'Í' : 'I2',
+	'Ì' : 'I3',
+	'Ú' : 'U2',
+	'Ù' : 'U3',
+	'Š' : 'C',
+	'Ĝ' : 'J',
+	'Ḫ' : 'H',
+	'Ṭ' : 'T.',
+	'Ṣ' : 'S.',
+}
+
+def sanitize_name(orig): # Sanitize a name so FontForge doesn't complain
+	out = 'LIG_' + orig
+	for k,v in SANITIZE.items():
+		out = out.replace(k,v)
+	print(f'\tSanitized {orig} to {out}')
+	return out
+
+ERROR_CODE = parser.parse('*') # A big X in the current renderer's style (since that's the default rendering of a wildcard stroke) - could change to P* to make it narrower if desired
+
 class Font:
-	def __init__(self, tmpname=Path('tmp.svg'), final_margin=100, initial_margin=100, final_bottom=200, glyph_size=1000, stroke_width=0.025, renderer=TwoSidedRenderer):
+	def __init__(self, tmpname=Path('tmp.svg'), final_margin=100, initial_margin=100, final_bottom=200, glyph_size=1000, stroke_width=0.025, renderer=TwoSidedRenderer, **extra):
 		self.font = fontforge.font() # Make a new font
 		self.tmp = tmpname
 		self.final_margin = final_margin
@@ -17,13 +51,38 @@ class Font:
 		self.glyph_size = glyph_size
 		self.stroke_width = stroke_width
 		self.renderer = renderer
+		
+		self.extra = extra
+		
+		self.used_in_ligatures = set()
+		self.used_outside_ligatures = set()
+		
+		self.make_ligature_tables()
+	
+	def make_ligature_tables(self):
+		self.subtable = 'ligsubtable'
+		featuple = ("liga", (("latn","dflt"),("xsux","dflt")) ) # Xsux is script code for cuneiform, but for some reason certain codepoints are assigned to Latn instead
+		self.font.addLookup('ligatures', 'gsub_ligature', None, (featuple,))
+		self.font.addLookupSubtable('ligatures', self.subtable)
 	
 	def finalize(self, filename=Path('font.sfd')):
 	#	self.font.generate(filename)
+		missing = self.used_in_ligatures - self.used_outside_ligatures
+		if missing: # If this becomes a problem, put actual handling here
+			print('\tWARNING: some signs used in ligatures but not outside them! Encoding as crosses for now...')
+			print('\t\t' + ' '.join(f'{c:04x}' for c in missing))
+			for cp in missing:
+				self.encode_glyph(cp, ERROR_CODE, None) # Creates the missing glyphs but leaves them empty
 		self.font.save(filename)
 	
 	def select_glyph(self, codepoint):
-		self.glyph = self.font.createChar(codepoint)
+		self.glyph = self.font.createChar(codepoint, glyph_name(codepoint))
+		self.used_outside_ligatures.add(codepoint)
+	
+	def select_ligature(self, cps, name):
+		self.glyph = self.font.createChar(-1, sanitize_name(name)) # Make a new character with no Unicode codepoint associated
+		self.glyph.addPosSub(self.subtable, tuple(glyph_name(cp) for cp in cps))
+		self.used_in_ligatures |= set(cps)
 	
 	def inkscape_processing(self):
 		actions = [
@@ -49,8 +108,8 @@ class Font:
 	def read_glyph_data(self):
 		self.glyph.importOutlines(str(self.tmp), scale=False)
 	#	print(self.glyph.boundingBox())
-		x1, y1, x2, y2 = self.glyph.boundingBox()
-		dx, dy = x2-x1, y2-y1
+	#	x1, y1, x2, y2 = self.glyph.boundingBox()
+	#	dx, dy = x2-x1, y2-y1
 		# Goal: y1 = 0, y2 = self.glyph_size, x1 = self.final_margin
 		# So first: scale by self.glyph_size / dy
 		# This is the size the SVG is rendered at, but FontForge scales things down so the *largest* dimension is no larger than 1000, which means most glyphs don't line up properly
@@ -70,46 +129,65 @@ class Font:
 		self.glyph.autoHint()
 	
 	def write_glyph_data(self, root):
-		data = self.renderer.render(root, scale=self.glyph_size, margin=self.initial_margin, strokewidth=self.stroke_width, fgcolor='black', bgcolor='0', format='svg').get_raw_data() # fgcolor and bgcolor are necessary for the SVG to be read properly
+		data = self.renderer.render(root, scale=self.glyph_size, margin=self.initial_margin, fgcolor='black', bgcolor='0', format='svg', **self.extra).get_raw_data() # fgcolor and bgcolor are necessary for the SVG to be read properly
 		with open(self.tmp, 'wb') as f: f.write(data.read()) # Transfer the buffer to an actual file because we need Inkscape and FontForge to be able to access it from the command line
 	
-	def encode_glyph(self, unicode, root):
-		if self.font.findEncodingSlot(unicode) != -1: return # Already exists
-		self.select_glyph(unicode)
+	def encode_glyph(self, unicode, root, name):
+		if isinstance(unicode, int): unicode = (unicode,) # Ensure a tuple
+		if len(unicode) == 1: # Single codepoint
+			if self.font.findEncodingSlot(unicode[0]) != -1: # Already exists
+				print(f'\tWarning: codepoint {unicode[0]:04x} already used! Skipping.')
+				return # Skip instead of overwriting - if we import another glyph into the slot it'll actually add them together instead of overwriting, and since the first one's already been repositioned, the result will be a mess
+			self.select_glyph(unicode[0])
+		else: # Ligature of codepoints
+			self.select_ligature(unicode, name)
 		self.write_glyph_data(root)
 		self.inkscape_processing()
 		self.read_glyph_data()
 
-def generate_font(renderer):
+def generate_font(renderer, outname, **extra):
 	from database import Database
 	db = Database()
 	db.load_data('data/hzl.dat')
 	print('Database loaded')
 	
-	font = Font(renderer=renderer)
+	font = Font(renderer=renderer, **extra)
 	print('Font skeleton prepared')
 	
 	import csv
-	with Path('data/unicode.csv').open('r', newline='') as f:
+	with Path('data/unicode_cleaned.csv').open('r', newline='') as f:
 		r = csv.DictReader(f)
-		for row in tqdm(list(r), disable=True):
+		for row in r:
 			hzl = row['HethZL'].strip()
+			if '*' in hzl: # Flags like *B, *C, etc are used when multiple Unicode codepoints should have the same HZL value (due to signs merging in Hittite)
+				hzl2 = hzl.split('*')[0]
+			else:
+				hzl2 = hzl
 			name = row['Sign Name'].strip()
-			tmp = row['Unicode Glyph'].strip()
-			if len(tmp.split()) == 2 and tmp.startswith('U+') and hzl: # This is a valid single glyph which has an HZL index
-				uni = tmp.split()[0][2:]
-				print(f'Looking up sign {hzl}: {name} ({uni})')
-				entry = next((e for e in db.data if e.ident==hzl), None)
-				if entry is None:
-					print(f'Warning: no sign numbered {hzl} found in database! Skipping and moving on')
-					continue
-				try:
-					font.encode_glyph(int(uni, 16), parser.parse(entry.forms[0]))
-				except KeyboardInterrupt:
-					break
+			unicode = row['Unicode Glyph'].strip()
+			
+			if not hzl or not unicode: continue
+			codepoints = []
+			for c in unicode.split():
+				if c.startswith('U+'):
+					codepoints.append(int(c[2:], 16)) # Read as hex
+			printable = '+'.join(f'{c:04x}' for c in codepoints)
+			if not codepoints:
+				print(f'\tWarning: no codepoints found for {name} ("{unicode}")')
+				continue
+			
+			print(f'Looking up sign {hzl}: {name} ({printable})')
+			entry = next((e for e in db.data if e.ident==hzl2), None)
+			if entry is None:
+				print(f'\tWarning: no sign numbered {hzl2} found in database! Skipping and moving on')
+				continue
+			try:
+				font.encode_glyph(codepoints, parser.parse(entry.forms[0]), name)
+			except KeyboardInterrupt:
+				break
 	print('Glyphs encoded')
 	
-	font.finalize('tmp.sfm')
+	font.finalize(outname)
 	print('Font exported! Finished!')
 
 if __name__ == '__main__':
@@ -118,5 +196,5 @@ if __name__ == '__main__':
 #	f.tmp = Path('/home/daniel/Downloads/tmp/ezen4.svg')
 #	f.glyph_import()
 #	f.finalize('/home/daniel/Downloads/tmp/tmp.sfd')
-	input()
-	generate_font(TwoSidedRenderer)
+#	input()
+	generate_font(InkRenderer, 'ink.sfd', strokewidth=0.05)
